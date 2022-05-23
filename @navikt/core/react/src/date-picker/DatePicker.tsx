5b1ae5004ac3ea9c9ca559daf07de9ccc3a96e33
Lars Hansen <lars.eirik.korsgaard.hansen@nav.no>
2022-05-23 11:49:04 +0200

    WIP: styling disabled days + outside days, update pkg

--- @navikt/core/react/src/date-picker/DatePicker.tsx
+++ @navikt/core/react/src/date-picker/DatePicker.tsx
@@ -3,28 +3,37 @@ import { addMonths } from "date-fns";
 import { DateRange, DayPicker } from "react-day-picker";
 import Footer from "./Footer";
 import datePickerClassNames from "./datePickerClassNames";
+import { addDays } from "date-fns/esm";
 
 export const DatePicker = () => {
   // TODO: allow both Date and iso-string from user
-  const pastMonth = new Date("2022-04-17");
+  const april_3 = new Date("2022-04-03");
+  const april_17 = new Date("2022-04-17");
 
+  const disabledDays: DateRange = {
+    from: april_3,
+    to: addDays(april_3, 5),
+  };
   const defaultSelected: DateRange = {
-    from: pastMonth,
-    to: addMonths(pastMonth, 1),
+    from: april_17,
+    to: addMonths(april_17, 1),
   };
 
   const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
 
   return (
     <DayPicker
+      showWeekNumber
       captionLayout="dropdown"
       mode="range"
+      disabled={disabledDays}
       classNames={datePickerClassNames}
-      defaultMonth={pastMonth}
+      defaultMonth={april_3}
       selected={range}
       footer={<Footer range={range} />}
       numberOfMonths={2}
       onSelect={setRange}
+      showOutsideDays
     />
   );
 };
