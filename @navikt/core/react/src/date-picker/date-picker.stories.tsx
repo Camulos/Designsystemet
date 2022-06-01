346f43cc45f0e1be174eafb561ee0f4a846be852
Lars Hansen <lars.eirik.korsgaard.hansen@nav.no>
2022-06-01 17:07:09 +0200

    remove unused variables

--- @navikt/core/react/src/date-picker/date-picker.stories.tsx
+++ @navikt/core/react/src/date-picker/date-picker.stories.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { DatePicker, DateRange, addDays, addMonths } from "./index";
+import { DatePicker, DateRange, addDays } from "./index";
 
 export default {
   title: "ds-react/date-picker",
@@ -8,7 +8,6 @@ export default {
 
 export const All = () => {
   const april_3 = new Date("2022-04-03");
-  const april_17 = new Date("2022-04-17");
 
   const disabledDays = {
     from: april_3,
@@ -17,12 +16,6 @@ export const All = () => {
 
   // Can we avoid that the user has to add type (DateRange)?
   const [selected, setSelected] = useState<DateRange>();
-
-  /* ranged config */
-  const defaultSelected = {
-    from: april_17,
-    to: addMonths(april_17, 1),
-  };
 
   return (
     <>
