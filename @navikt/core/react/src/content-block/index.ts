352a8903714feaf2c0fc22b1b0b3566902ce97c9
Ken <ken.aleksander@gmail.com>
2022-10-28 15:10:20 +0200

    :art: ContentBlock

---
+++ @navikt/core/react/src/content-block/index.ts
@@ -0,0 +1,1 @@
+export { default as ContentBlock, ContentBlockProps } from "./ContentBlock";
--- @navikt/core/react/src/index.ts
+++ @navikt/core/react/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./accordion";
 export * from "./alert";
+export * from "./content-block";
 export * from "./button";
 export * from "./date";
 export * from "./form";
